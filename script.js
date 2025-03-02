b268b15a68ac107f9c2b82dc275ae4b1fa27c30a
Veicm <connormannes@gmx.de>
2025-03-02 18:42:05 +0100

    update

--- script.js
+++ script.js
@@ -1,18 +1,49 @@
-const display = document.getElementById('display');
+const display = document.getElementById("display");
+let number = "";
 
-function appendToDisplay(input){
-    display.value += input;
+function appendToDisplay(input) {
+  display.value += input;
+  number += input;
 }
 
-function clearDisplay(){
-    display.value = "";
+function clearDisplay() {
+  display.value = "";
+  number = "";
 }
 
-function calculate(){
-    try {
-        display.value = eval(display.value);
+function perCent() {
+  let lastNumberMatch = display.value.match(/(\d+)(?!.*\d)/);
+  if (lastNumberMatch) {
+    let lastNumber = lastNumberMatch[0];
+    let newNumber = lastNumber / 100;
+    display.value = display.value.slice(0, -lastNumber.length) + newNumber;
+  } 
+}
+
+function invert(exist) {
+  let lastNumber = parseFloat(exist[0]);
+  if (lastNumber > 0) {
+    let newNumber = `(${(-lastNumber).toString()})`;
+    display.value = display.value.slice(0, -lastNumber.toString().length) + newNumber;
+  }
+}
+
+function negative() {
+    let lastNumberMatch = display.value.match(/(\d+)(?!.*\d)/);
+    if (lastNumberMatch) {
+        invert(lastNumberMatch);
     }
-    catch (error) {
-        display.value = "Error";
-    }
+}
+
+function addCalc(input) {
+  display.value += input;
+  number = "";
+}
+
+function calculate() {
+  try {
+    display.value = eval(display.value);
+  } catch (error) {
+    display.value = "Error";
+  }
 }
