a15f27f9a5de2fad1d1da547b6e8e25d3f040cac
Veicm <connormannes@gmx.de>
2025-03-02 18:55:33 +0100

    update

--- script.js
+++ script.js
@@ -17,22 +17,23 @@ function perCent() {
     let lastNumber = lastNumberMatch[0];
     let newNumber = lastNumber / 100;
     display.value = display.value.slice(0, -lastNumber.length) + newNumber;
-  } 
+  }
 }
 
 function invert(exist) {
   let lastNumber = parseFloat(exist[0]);
   if (lastNumber > 0) {
     let newNumber = `(${(-lastNumber).toString()})`;
-    display.value = display.value.slice(0, -lastNumber.toString().length) + newNumber;
+    display.value =
+      display.value.slice(0, -lastNumber.toString().length) + newNumber;
   }
 }
 
 function negative() {
-    let lastNumberMatch = display.value.match(/(\d+)(?!.*\d)/);
-    if (lastNumberMatch) {
-        invert(lastNumberMatch);
-    }
+  let lastNumberMatch = display.value.match(/(\d+)(?!.*\d)/);
+  if (lastNumberMatch) {
+    invert(lastNumberMatch);
+  }
 }
 
 function addCalc(input) {
